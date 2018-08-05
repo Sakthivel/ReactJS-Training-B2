eccc82e2f90603247faafb50a41c644e308ba9e7
Sakthivel Sengodan <ssengodan@sapient.com>
2018-08-05 20:27:20 +0530

    Keep App exercise

---
+++ week4/testapp/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import registerServiceWorker from './registerServiceWorker';
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
