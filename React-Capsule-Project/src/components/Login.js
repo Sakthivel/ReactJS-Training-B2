0fee852e6ae0d301da9f9019442dda65c5550bfe
Sakthivel Sengodan <ssengodan@sapient.com>
2018-09-09 15:57:07 +0530

    New style added

--- React-Capsule-Project/src/components/Login.js
+++ React-Capsule-Project/src/components/Login.js
@@ -3,8 +3,18 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-
-
+const styles = {
+    h1: {
+        textAlign: 'center'
+    },
+    layout: {
+        border: '1px solid darkblue',
+        padding: '20px 20px 70px',
+        backgroundColor: 'lightskyblue',
+        marginTop: '20px',
+        color: 'darkblue'
+    }
+}
 class Login extends Component {
     constructor(props) {
         super();
@@ -26,22 +36,27 @@ class Login extends Component {
     render() {
         return (
              <Grid container spacing={24}>
-                <Grid item xs={4}>
-                    <h1>Talking App</h1>
+                <Grid item xs={4}></Grid>
+                <Grid item xs={4} style={styles.layout}>
+                    <h1 style={styles.h1}>Talking App</h1>
+                    <form onSubmit={this.onSubmit.bind(this)} >
+                        <Typography variant="title" color="inherit">Sign In</Typography>
+                        <TextField
+                            id="name"
+                            label="Username"
+                            onChange={this.onChange.bind(this)}
+                            margin="normal"
+                            fullWidth
+                            />
+                        <Button onClick={this.onSubmit.bind(this)} 
+                                variant="raised"
+                                fullWidth 
+                                color="primary" >
+                            Submit
+                        </Button>
+                    </form>
                 </Grid>
-                <Grid item xs={8}>
-                        <form onSubmit={this.onSubmit.bind(this)} >
-                            <Typography variant="title" color="inherit">Login</Typography>
-                            <TextField
-                                id="name"
-                                label="Username"
-                                onChange={this.onChange.bind(this)}
-                                margin="normal"
-                                />
-                            <Button onClick={this.onSubmit.bind(this)} variant="outlined" color="primary" >
-                                Submit
-                            </Button>
-                        </form>
+                <Grid item xs={4}>
                 </Grid>
             </Grid>
         )
--- React-Capsule-Project/src/components/test/Login.test.js
+++ React-Capsule-Project/src/components/test/Login.test.js
@@ -8,26 +8,24 @@ import {
 } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai';
-
-//import TextField from '@material-ui/core/TextField';
+import TextField from '@material-ui/core/TextField';
 
 configure({
     adapter: new Adapter()
 });
 
 describe("<login />", function() {
-  xit('should render <Login /> component title section', function() {
-    const login = shallow( <Login /> );
-    console.log(login.html());
-    //expect(login.html().find('#signin')).to.have.length(1);
-    expect(login.find('.Login-layout-1')).to.have.length(1);
-    //expect(login.find('h1').text()).to.equal('Talking App');
-  });
+    it('should render <Login /> component title section', function() {
+        const login = shallow( <Login /> );
+        expect(login.find('h1')).to.have.length(1);
+        expect(login.find('h1').type()).to.be.a('string');
+        expect(login.find('h1').text()).to.equal('Talking App');
+    });
 
-  it('should render <Login /> component form section', function () {
-      const login = shallow( <Login /> );
-      expect(login.find('form')).to.have.length(1);
-      expect(login.find('#name')).to.have.length(1);
-      //expect(login.find('#name').type()).to.equal(Input);
-  });
+    it('should render <Login /> component form section', function () {
+        const login = shallow( <Login /> );
+        expect(login.find('form')).to.have.length(1);
+        expect(login.find('#name')).to.have.length(1);
+        expect(login.find('#name').type()).to.equal(TextField);
+    });
 });
